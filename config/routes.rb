e2ad7764598db17b67f01c6446fb0102b18acd2d
midoridougan <midori.dougan813@gmail.com>
2020-07-24 22:33:59 +0900

    re05

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # config = Rails.application.config.baukis2
+
+  # constraints host: config[:staff][:host] do
+    namespace :staff do
+      root "top#index"
+      # get "login" => "sessions#new", as: :login
+      # resource :session, only: [ :create, :destroy ]
+      # resource :account, except: [ :new, :create, :destroy ]
+      # resource :password, only: [ :show, :edit, :update ]
+      # resources :customers
+    end
+  # end
+
+  # constraints host: config[:admin][:host] do
+    namespace :admin do
+      root "top#index"
+      # get "login" => "sessions#new", as: :login
+      # resource :session, only: [ :create, :destroy ]
+      # resources :staff_members do
+      #   resources :staff_events, only: [ :index ]
+      # end
+      # resources :staff_events, only: [ :index ]
+    end
+  # end
+
+  # constraints host: config[:customer][:host] do
+    namespace :customer do
+      root "top#index"
+    end
+  # end
 end
