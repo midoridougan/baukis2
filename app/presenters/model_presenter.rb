387b01487dc3ba320dab95e66fe09f5f665279b0
midoridougan <midori.dougan813@gmail.com>
2020-08-01 06:14:56 +0900

    15-1 commit

---
+++ app/presenters/model_presenter.rb
@@ -0,0 +1,9 @@
+class ModelPresenter
+  attr_render :object, :view_context
+  delegate :raw, to: :view_context
+
+  def initialize(object, view_context)
+    @object = object
+    @view_context = view_context
+  end
+end
