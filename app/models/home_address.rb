578e4a729091e193c85865d8d0fac64b79c7fcaf
midoridougan <midori.dougan813@gmail.com>
2020-08-07 23:15:09 +0900

    Allcomplete

--- app/models/home_address.rb
+++ app/models/home_address.rb
@@ -1,4 +1,4 @@
-class HomeAddress < HomeAddress
+class HomeAddress < Address
   validates :postal_code, :prefecture, :city, :address1, presence: true
   validates :company_name, :division_name, absence: true
 end
